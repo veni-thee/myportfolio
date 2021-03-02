62c29ec04b7e5a0e34a448750db4dc4c65fa6ad8
Veni Gonzales <venibonnie@gmail.com>
2021-03-02 01:24:31 -0700

    adding files

---
+++ script.js
@@ -0,0 +1,38 @@
+function openInfo(tabName, elmnt, color) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Remove the background color of all tablinks/buttons
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+
+  // Show the specific tab content
+  document.getElementById(tabName).style.display = "block";
+
+  // Add the specific color to the button used to open the tab content
+  elmnt.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+
+//slideshow
+fetch('https://api.unsplash.com/photos/random/?count=5&client_id=52d8369eb3e2576a5f5b6423865e074e9c7045761bff1ac5664ff3e0bdb57a1d') 
+  .then(response => response.json())
+  .then(data => {
+    data.forEaach(function(image, i) {
+      document.querySelector("#slide-" + (i+1)).innerHTML = `
+        <img src="${image.urls.regular}" alt="">
+        <p class="author-info">
+          <a href="${image.links.html}?utm_source=slider-thing&utm_medium=referral&utm_campaign=api-credit">Photo by ${image.user.name}</a> on <a href="https://unsplash.com/">Unsplash</a>
+        </p>
+      `;
+    });
+  });
